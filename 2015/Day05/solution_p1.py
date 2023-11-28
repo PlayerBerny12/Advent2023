592b5716790c4272f9225f32b518f01eb0a440c4
PlayerBerny12 <ber.honza12@seznam.cz>
2023-11-28 08:46:29 +0000

    2015 - 05

---
+++ 2015/Day05/solution_p1.py
@@ -0,0 +1,23 @@
+import fileinput
+
+nice_count = 0
+
+for line in fileinput.input():
+    vowel_count = 0
+    double_letter = False
+    previous_char = ''
+    
+    if "ab" not in line and "cd" not in line and "pq" not in line and "xy" not in line:
+        for char in line:
+            if previous_char == char:
+                double_letter = True
+            
+            previous_char = char
+
+            if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+                vowel_count += 1
+
+        if double_letter and vowel_count >= 3:
+            nice_count += 1
+
+print(nice_count)
---
+++ 2015/Day05/solution_p2.py
@@ -0,0 +1,33 @@
+import fileinput
+
+nice_count = 0
+
+for line in fileinput.input():    
+    double_letter = False    
+    previous_chars = ['', '']
+    
+    for char in line:
+        if previous_chars[1] == char:
+            double_letter = True
+        
+        previous_chars[1] = previous_chars[0]
+        previous_chars[0] = char
+
+        
+    for i in range(len(line) - 1):
+        double_char = line[i:i+2]
+        pair_count = 0
+
+        iterator = iter(range(len(line) - 1))
+        for j in iterator:
+            if line[j:j+2] == double_char:
+                pair_count += 1                
+                next(iterator, None)
+
+        if pair_count >= 2:
+            break
+
+    if double_letter and pair_count >= 2:
+            nice_count += 1
+
+print(nice_count)
