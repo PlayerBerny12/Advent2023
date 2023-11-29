4f76dcd3a71f65df8c044cb74f04ac81f736a561
PlayerBerny12 <ber.honza12@seznam.cz>
2023-11-29 09:21:15 +0000

    2015 - 06

---
+++ 2015/Day06/solution_p1.py
@@ -0,0 +1,32 @@
+import sys
+import re
+from enum import Enum
+
+class Command(str, Enum):
+    TURNON = 'turn on'
+    TOGGLE = 'toggle'
+    TURNOFF = 'turn off'
+
+grid = [[0 for _ in range(1000)] for _ in range(1000)]
+instructions = re.findall(r"(turn on|toggle|turn off)\s(\d+),(\d+)\sthrough\s(\d+),(\d+)", sys.stdin.read())
+
+for commnad, x1, y1, x2, y2 in instructions:
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2) + 1
+    y2 = int(y2) + 1
+    
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            if commnad == Command.TURNON:
+                grid[x][y] = 1
+            elif commnad == Command.TOGGLE:
+                grid[x][y] = 1 - grid[x][y]
+            elif commnad == Command.TURNOFF:
+                grid[x][y] = 0
+
+count = 0
+for line in grid:
+    count += sum(line)
+    
+print(count)
